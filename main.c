f4b06bd27a3bf480d1d1a43dbc83f65d3d90d62b
Spike <hjpg20@gmail.com>
2025-12-15 18:16:14 -0500

    Adds display mode structure and population

--- main.c
+++ main.c
@@ -5,12 +5,28 @@
 
 bool is_running = false;
 
+SDL_DisplayMode display_mode;
+
+uint32_t window_width = 0;
+uint32_t window_height = 0;
+
 bool initialize_window(void) {
 
 	if (SDL_Init(SDL_INIT_EVERYTHING)) {
 		fprintf(stderr, "Failed to initialize SDL.\n");
 		return false;
 	}
+
+	if (SDL_GetCurrentDisplayMode(0, &display_mode)) {
+		fprintf(stderr, "Failed to get current display mode.\n");
+		return false;
+	}
+
+	window_width = display_mode.w;
+	window_height = display_mode.h;
+
+
+
 	return true;
 }
 
