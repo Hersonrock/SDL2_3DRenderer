84d0771c46823bd6f8497bb4bfa19596f11ba40f
Spike <hjpg20@gmail.com>
2025-12-15 21:12:47 -0500

    Adding input handling

--- main.c
+++ main.c
@@ -53,6 +53,23 @@ void setup(void) {
 }
 
 void process_input(void) {
+	SDL_Event event;
+	SDL_PollEvent(&event);
+
+	switch (event.type) {
+		
+		case SDL_QUIT:
+			is_running = false;
+			break;
+
+		case SDL_KEYDOWN:
+			if (event.key.keysym.sym == SDLK_ESCAPE) {
+				is_running = false;
+			}
+			break;
+		default:
+			break;
+	}
 
 }
 
