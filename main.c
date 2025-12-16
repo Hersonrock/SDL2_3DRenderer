0246dd90d64128ffb9fc046ab25dabaa13469f68
Spike <hjpg20@gmail.com>
2025-12-16 07:39:49 -0500

    Adds pixel and line draw functions

--- main.c
+++ main.c
@@ -5,6 +5,7 @@
 
 #define FPS 30
 #define FRAME_TARGET_TIME (1000 / FPS)
+#define PI 3.14159265358979323846
 
 bool is_running = false;
 
@@ -89,6 +90,69 @@ void update(void) {
 	previous_frame_time = SDL_GetTicks();
 }
 
+void draw_pixel(uint32_t x, uint32_t y, uint32_t color) {
+	color_buffer[(window_width * y) + x] = color;
+}
+
+void draw_line(uint32_t x1, uint32_t y1, uint32_t x2, uint32_t y2, uint32_t color) {
+
+	int32_t dx = abs(x1 - x2);
+	int32_t dy = abs(y1 - y2);
+
+	int32_t sx = (x1 < x2) ? 1 : -1;
+	int32_t sy = (y1 < y2) ? 1 : -1;
+
+	int32_t err = dx - dy;
+
+	while (1) {
+		draw_pixel(x1, y1, color);
+
+		if (x1 == x2 && y1 == y2) break;
+
+		int32_t e2 = err << 1;
+
+		if (e2 > -dy) {
+			err -= dy;
+			x1 += sx;
+		}
+		if (e2 < dx) {
+			err += dx;
+			y1 += sy;
+		}
+	}
+}
+
+int circle_line_test(void) {
+
+	uint32_t cx = window_width / 2;
+	uint32_t cy = window_height / 2;
+	uint32_t r = (window_height - window_height * 0.1) / 2;
+	uint32_t white = 0xFFFFFFFF;
+
+	//There should be 16 lines, starting at the center of the screen and ending at the edge of a circle. Let see
+	draw_line(cx, cy, cx, cy - r, white);
+	draw_line(cx, cy, cx, cy + r, white);
+	draw_line(cx, cy, cx + r, cy, white);
+	draw_line(cx, cy, cx - r, cy, white);
+
+	draw_line(cx, cy, cx + r * cos(PI / 4), cy - r * sin(PI / 4), white);
+	draw_line(cx, cy, cx + r * cos(PI / 4), cy + r * sin(PI / 4), white);
+	draw_line(cx, cy, cx - r * cos(PI / 4), cy + r * sin(PI / 4), white);
+	draw_line(cx, cy, cx - r * cos(PI / 4), cy - r * sin(PI / 4), white);
+
+	draw_line(cx, cy, cx + r * cos(PI / 3), cy - r * sin(PI / 3), white);
+	draw_line(cx, cy, cx + r * cos(PI / 6), cy + r * sin(PI / 6), white);
+	draw_line(cx, cy, cx - r * cos(PI / 6), cy + r * sin(PI / 6), white);
+	draw_line(cx, cy, cx - r * cos(PI / 3), cy - r * sin(PI / 3), white);
+
+	draw_line(cx, cy, cx + r * cos(PI / 6), cy - r * sin(PI / 6), white);
+	draw_line(cx, cy, cx + r * cos(PI / 3), cy + r * sin(PI / 3), white);
+	draw_line(cx, cy, cx - r * cos(PI / 3), cy + r * sin(PI / 3), white);
+	draw_line(cx, cy, cx - r * cos(PI / 6), cy - r * sin(PI / 6), white);
+
+	return 0;
+}
+
 void color_buffer_clear(uint32_t color){
 	for (size_t y = 0; y < window_height; y++) {
 		for (size_t x = 0; x < window_width; x++) {
@@ -109,6 +173,7 @@ void render(void) {
 	SDL_SetRenderDrawColor(renderer,0,100,100,255);
 	SDL_RenderClear(renderer);
 
+	circle_line_test();
 	color_buffer_render();
 	color_buffer_clear(0xFF550000);
 
