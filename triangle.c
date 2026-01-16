26a255f06f48b1ea70660f9b19de25c06ead6ceb
HernanP <hjpg20@gmail.com>
2026-01-15 21:56:09 -0500

    Adds triangle m and vertices sort

--- triangle.c
+++ triangle.c
@@ -1,1 +1,46 @@
 #include "triangle.h"
+#include <stdbool.h>
+#include <math.h>
+
+vec2_t triangle_get_m(triangle_t* triangle) {
+
+	vec2_t m = { 0 };
+	m.y = triangle->points[1].y;
+
+	float y0 = triangle->points[0].y;
+	float x0 = triangle->points[0].x;
+
+	float y1 = triangle->points[2].y;
+	float x1 = triangle->points[2].x;
+
+	float dy = y1 - y0;
+
+	if (fabs(dy) > EPSILON) {
+		float t = (m.y - y0) / dy;
+		m.x = x0 + t * (x1 - x0);
+	}
+	else {
+		m.x = x0;
+	}
+
+	return m;
+}
+
+void sort_triangle_vertices_on_y(triangle_t* triangle) {
+	bool swap;
+	while (true) {
+		swap = false;
+		if (triangle->points[1].y < triangle->points[0].y) {
+			vect2_swap(&triangle->points[1], &triangle->points[0]);
+			swap = true;
+		}
+
+		if (triangle->points[2].y < triangle->points[1].y) {
+			vect2_swap(&triangle->points[1], &triangle->points[2]);
+			swap = true;
+		}
+		if (!swap) {
+			break;
+		}
+	}
+}
--- triangle.h
+++ triangle.h
@@ -1,5 +1,6 @@
 #ifndef  TRIANGLE_H
 #define TRIANGLE_H
+#define EPSILON        1e-8f
 
 #include "vector.h"
 
@@ -8,5 +9,8 @@
 typedef struct {
 	vec2_t points[TRI];
 }triangle_t;
+
+void sort_triangle_vertices_on_y(triangle_t* triangle);
+vec2_t triangle_get_m(triangle_t* triangle);
 
 #endif // TRIANGLE_H
